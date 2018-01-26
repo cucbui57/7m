9f10d31e6f25e298a9390b23220a6480652714e3
Pham Dai <pmd.ptit@gmail.com>
2018-01-26 10:41:20 +0700

    test

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+
+// Khai báo các đường dẫn tới thư mục
+define('app_path', __DIR__);
+define('layout_path', app_path . '/View/Layout/');
+define('controller_path', app_path . '/Controller/');
+define('model_path', app_path . '/Model/');
+
+//Khởi chạy
+
+require_once app_path . '/Core/app.php';
+require_once app_path . '/libs/functions.php';
+include app_path . '/config/config.php';
+
+$app = new myMVC();
+$app->run();
+
+//Trong 10 phút mà không thực hiện xong đơn hàng
+$inactive = 600;
+if (!isset($_SESSION['timeout'])) {
+    $_SESSION['timeout'] = time() + $inactive;
+}
+$session_life = time() - $_SESSION['timeout'];
+if ($session_life > $inactive) {
+    session_destroy();
+}
+
+$_SESSION['timeout'] = time();
